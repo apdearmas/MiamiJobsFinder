839574986ff2ff2f1dcec6ca16dfb718a6775a61
Antonio Pereira <apdearmas@yahoo.com>
2015-02-17 19:03:31 -0500

    Rename DefaultConnection to JobFinderContext

--- MiamiJobsFinder/Models/IdentityModels.cs
+++ MiamiJobsFinder/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace MiamiJobsFinder.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("JobFinderContext", throwIfV1Schema: false)
         {
         }
 
