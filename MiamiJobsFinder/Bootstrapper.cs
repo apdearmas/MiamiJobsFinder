f1da0570b3bb18d2e444bdea14454b41db6adaca
Antonio Pereira <apdearmas@yahoo.com>
2015-02-18 20:07:42 -0500

    Added dependency injection framewok

---
+++ MiamiJobsFinder/Bootstrapper.cs
@@ -0,0 +1,41 @@
+using System.Web.Mvc;
+using BDL;
+using BusinessDomain;
+using DAL;
+using Microsoft.Practices.Unity;
+using Unity.Mvc5;
+
+namespace MiamiJobsFinder
+{
+  public static class Bootstrapper
+  {
+    public static IUnityContainer Initialise()
+    {
+      var container = BuildUnityContainer();
+
+      DependencyResolver.SetResolver(new UnityDependencyResolver(container));
+
+      return container;
+    }
+
+    private static IUnityContainer BuildUnityContainer()
+    {
+      var container = new UnityContainer();
+
+      RegisterTypes(container);
+
+      return container;
+    }
+
+    public static void RegisterTypes(IUnityContainer container)
+    {
+        container.RegisterType<ISendJobOffersService, SendJobOffersService>();
+        container.RegisterType<ICustomerService, CustomerService>();
+        container.RegisterType<IRepository<Customer>, Repository<Customer>>();
+        container.RegisterType<IJobOffersService, JobOffersService>();
+        container.RegisterType<IRepository<JobOffer>, Repository<JobOffer>>();
+        container.RegisterType<IEmailService, BDL.EmailService>();
+        container.RegisterType<ISmtpClientWrapper, SmtpClientWrapper>();
+    }
+  }
+}
--- MiamiJobsFinder/Controllers/SendEmailController.cs
+++ MiamiJobsFinder/Controllers/SendEmailController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Web.Mvc;
+using BDL;
 
 namespace MiamiJobsFinder.Controllers
 {
