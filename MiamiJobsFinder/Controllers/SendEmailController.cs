45ef614fbea068b81521f5c4600a56f1d4f12a07
Antonio Pereira <apdearmas@yahoo.com>
2015-02-18 19:32:33 -0500

    Added SendEmailController.

---
+++ MiamiJobsFinder/Controllers/SendEmailController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace MiamiJobsFinder.Controllers
+{
+    public class SendEmailController : Controller
+    {
+        // GET: SendEmail
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
