f32d09f3c879b135fcf9b50d09dbd7efa1ea1b49
fgongora <fgongora67@gmail.com>
2015-03-06 16:32:56 -0500

    Added Editor for Description field in JobOffers

--- BusinessDomain/JobOffer.cs
+++ BusinessDomain/JobOffer.cs
@@ -1,13 +1,20 @@
 ﻿using System;
+using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
+
 
 namespace BusinessDomain
 {
+ 
     public class JobOffer
     {
         public int Id { get; set; }
         public string Title { get; set; }
         public DateTime IssuedDate { get; set; }
         public DateTime ExpirationDate { get; set; }
+
+        [AllowHtml]
+        [UIHint("tinymce_full_compressed")]
         public string Description { get; set; }
         public ContactPerson ContactPerson { get; set; }
         public Location Location { get; set; }
