7dc9d687ca26dff1c36cc5029af9b4122d702c75
Antonio Pereira <apdearmas@yahoo.com>
2015-03-25 18:07:47 -0400

    Added test class

---
+++ MiamiJobsFinder.Tests/Controllers/ContactControllerTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+using MiamiJobsFinder.Controllers;
+using Xunit;
+using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+
+namespace MiamiJobsFinder.Tests.Controllers
+{
+    public class ContactControllerTest
+    {
+
+        [Fact]
+        public void Contact()
+        {
+            // Arrange
+            var controller = new ContactController();
+
+            // Act
+            var result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+    }
+}
