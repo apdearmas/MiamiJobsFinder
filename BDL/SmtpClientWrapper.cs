840b5af202586f0d58edaeb82856050b76708027
Antonio Pereira <apdearmas@yahoo.com>
2015-03-26 17:00:00 -0400

    Remove SendGrid credentials from project

--- BDL/SmtpClientWrapper.cs
+++ BDL/SmtpClientWrapper.cs
@@ -9,35 +9,7 @@ namespace BDL
     {
         public void Send(string targetEmail, string subject, string message)
         {
-            // Create network credentials to access your SendGrid account.
-            const string username = "apdearmas";
-            const string pswd = "dollar1Lexus";
-
-            var credentials = new NetworkCredential(username, pswd);
-
-
-            // Create the email object first, then add the properties.
-            var myMessage = new SendGridMessage();
-            myMessage.AddTo(targetEmail);
-            myMessage.From = new MailAddress("apdearmas@sendgrid.com", "Antonio Pereira");
-            myMessage.Subject = subject;
-            myMessage.Html = message;
-
-            //            using (var attachmentFileStream = new FileStream(@"C:\dev\Giraffas.pdf", FileMode.Open))
-            //            {
-            //                myMessage.AddAttachment(attachmentFileStream, "Giraffas.pdf");
-            //            }
-            //            using (var attachmentFileStream = new FileStream(@"C:\dev\lasership.pdf", FileMode.Open))
-            //            {
-            //                myMessage.AddAttachment(attachmentFileStream, "lasership.pdf");
-            //            }
-
-            // Create an Web transport for sending email.
-            var transportWeb = new Web(credentials);
-
-            // Send the email.
-            transportWeb.Deliver(myMessage);
-
+            
         }
     }
 }
