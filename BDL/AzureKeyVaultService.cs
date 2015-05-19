5451a0620c544bc6b9a79c099b5da8326bc47fa8
Antonio Pereira <apdearmas@yahoo.com>
2015-05-19 10:48:23 -0400

    Test for Malav

--- BDL/AzureKeyVaultService.cs
+++ BDL/AzureKeyVaultService.cs
@@ -10,10 +10,17 @@ namespace BDL
         private static AzureKeyVaultService _instance;
         private readonly KeyVaultClient keyVaultClient;
 
+        private readonly string sendGridUserName;
+        private readonly string sendGridPassword;
+
         private AzureKeyVaultService()
         {
             keyVaultClient = new KeyVaultClient(GetAccessToken);
+
+            sendGridUserName = GetSecret("https://miamijobsfinder.vault.azure.net:443/secrets/SendGridUserName/86cac771cd5c4c5ebe49d746c4237f72").Value;
+            sendGridPassword = GetSecret("https://miamijobsfinder.vault.azure.net:443/secrets/SendGridPassword/cd167b9f76fa4c998cda6bf52b4434fb").Value;
         }
+
 
         public static AzureKeyVaultService Instance
         {
@@ -24,39 +31,36 @@ namespace BDL
         public string SendGridUserName {
             get
             {
-                var secret =
-                    keyVaultClient.GetSecretAsync(
-                        "https://miamijobsfinder.vault.azure.net:443/secrets/SendGridUserName/86cac771cd5c4c5ebe49d746c4237f72")
-                        .GetAwaiter()
-                        .GetResult();
-                return secret.Value;
+                return sendGridUserName;
             }
         }
 
         public string SendGridPassword {
             get
             {
-                var secret =
-                    keyVaultClient.GetSecretAsync(
-                        "https://miamijobsfinder.vault.azure.net:443/secrets/SendGridPassword/cd167b9f76fa4c998cda6bf52b4434fb")
-                        .GetAwaiter()
-                        .GetResult();
-                return secret.Value;
+                return sendGridPassword;
             }
         }
 
 
         public static Task<string> GetAccessToken(string authority, string resource, string scope)
         {
-            var client_id = ConfigurationManager.AppSettings["AuthClientId"];
-            var client_secret = ConfigurationManager.AppSettings["AuthClientSecret"];
+            var clientId = ConfigurationManager.AppSettings["AuthClientId"];
+            var clientSecret = ConfigurationManager.AppSettings["AuthClientSecret"];
 
-            var clientCredential = new ClientCredential(client_id, client_secret);
+            var clientCredential = new ClientCredential(clientId, clientSecret);
             var context = new AuthenticationContext(authority, null);
             var result = context.AcquireToken(resource, clientCredential);
 
             return Task.FromResult(result.AccessToken);
         }
+
+        private Secret GetSecret(string url)
+        {
+            return keyVaultClient.GetSecretAsync(url).GetAwaiter().GetResult();
+
+        }
+
 
     }
 }
